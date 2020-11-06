2d6dc3bab920013430cbfb27b16949528bded16e
Cody Salyi <Cody.Salyi@hyland.com>
2020-11-06 16:43:12 -0500

    initial commit of Condensate.Domain and Condensate.Infra.Data projects

---
+++ src/Condensate.Domain/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Condensate.Domain.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
